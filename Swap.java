71fb15dc4f3ac672f4f12f0f47d8ce12af1e05d9
Archana <100863073+Archana1714@users.noreply.github.com>
2022-09-10 13:52:10 +0530

    Swapping

---
+++ Swap.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+class Swap
+{
+public static void main(String args[])
+{
+int a=10,b=20;
+System.out.println(a+" "+b);
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println(a+" "+b);
+}
+}
