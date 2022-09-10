4f1c3cfb761254bcd066cbf8cdb47565b469ffae
Archana <100863073+Archana1714@users.noreply.github.com>
2022-09-10 13:49:02 +0530

    Caculator

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+class Calculator
+{
+public static void main(String args[])
+{
+int a=10,b=20;
+System.out.println("Sum="+(a+b));
+System.out.println("Sub="+(a-b));
+System.out.println("Multi="+(a*b));
+System.out.println("Divide="+(a/b));
+}
+}
